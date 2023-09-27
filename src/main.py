b63039b74031c91510913844b0c9239d12e91753
fredrikofstad <nokotin@gmail.com>
2023-09-27 23:25:32 +0200

    first commit

---
+++ src/main.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import numpy as np
+
+data_path = "data/"
+
+# reading data
+solar_df = pd.read_csv(data_path + "SolarIrradiance.csv")
+wind_df = pd.read_csv(data_path + "WindSpeed.csv")
+consumption_df = pd.read_csv(data_path + "rate_consumption_charge.csv")
+
+# MegaWatt/km^2
+solar_irradiance = np.array(solar_df.iloc[:, 2])
+
+# km/h = 1/3.6 m/s
+wind_speed = 3.6 * np.array(wind_df.iloc[:, 3])
+
+# rate of consumption charge
+consumption_charge = np.array(consumption_df.iloc[:, 4])/10
+
+print(solar_irradiance)
+print(wind_speed)
+print(consumption_charge)
---
+++ src/microgrid.py
@@ -0,0 +1,59 @@
+import numpy as np
+import pandas as pd
+
+from pymgrid import Microgrid
+from pymgrid.modules import (
+    BatteryModule,
+    LoadModule,
+    RenewableModule,
+    GridModule)
+
+np.random.seed(5460)
+
+small_battery = BatteryModule(min_capacity=10,
+                              max_capacity=100,
+                              max_charge=50,
+                              max_discharge=50,
+                              efficiency=0.9,
+                              init_soc=0.2)
+
+large_battery = BatteryModule(min_capacity=10,
+                              max_capacity=1000,
+                              max_charge=10,
+                              max_discharge=10,
+                              efficiency=0.7,
+                              init_soc=0.2)
+
+load_ts = 100+100*np.random.rand(24*90)  # random load data in the range [100, 200].
+pv_ts = 200*np.random.rand(24*90)  # random pv data in the range [0, 200].
+
+load = LoadModule(time_series=load_ts)
+
+pv = RenewableModule(time_series=pv_ts)
+
+grid_ts = [0.2, 0.1, 0.5] * np.ones((24*90, 3))
+
+grid = GridModule(max_import=100,
+                  max_export=100,
+                  time_series=grid_ts)
+
+modules = [
+    small_battery,
+    large_battery,
+    ('pv', pv),
+    load,
+    grid]
+
+microgrid = Microgrid(modules)
+
+#print(microgrid)
+#print(microgrid.modules.pv)
+#print(microgrid.modules.grid is microgrid.modules['grid'])
+
+# print(microgrid.controllable)
+
+# All none have to be replaced to control the microgrid
+# print(microgrid.get_empty_action())
+
+microgrid.reset()
+print(microgrid.state_series().to_frame())
